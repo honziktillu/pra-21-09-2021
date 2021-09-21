c58475605457e68c13b36fd579eb857f2a46f22b
Jan Till <till@spsmb.cz>
2021-09-21 08:22:32 +0200

    idk proc ho delam lol

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] validSymbols = {"s", "o", "n", "d"};
+        boolean isValid = false;
+        for (String validSymbol : validSymbols) {
+            if (validSymbol.equals(args[0])) {
+                isValid = true;
+            }
+        }
+        if (!isValid) {
+            System.out.println("Bad arg");
+            System.exit(-1);
+        }
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        switch (args[0]) {
+            case "s":
+                System.out.println(a + b);
+                break;
+            case "o":
+                System.out.println(a - b);
+                break;
+            case "n":
+                System.out.println(a * b);
+                break;
+            case "d":
+                System.out.println(a / b);
+                break;
+        }
+
+    }
+}
